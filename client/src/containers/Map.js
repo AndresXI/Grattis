a786bf337c0b1c51ae34a7e6477cf3bbaa2c136e
andres alcocer <andresalcocer7@yahoo.com>
2019-05-06 17:15:12 -0400

    initialized map with current location

--- client/src/containers/Map.js
+++ client/src/containers/Map.js
@@ -2,27 +2,36 @@ import React, { Component } from 'react';
 
 export default class Map extends Component {
   componentDidMount() {
-    const script = document.createElement('script');
-    script.src = `${process.env.PUBLIC_URL}/sdk/tomtom.min.js`;
-    document.body.appendChild(script);
-    script.async = true;
-    script.onload = function () {
-      window.tomtom.L.map('map', {
-        source: 'vector',
-        key: '01ZXmKWLDr1TSBvi86xEvfIBv8DkMSX7',
-        center: [37.769167, -122.478468],
-        basePath: '/sdk',
-        zoom: 15,
-      });
-    };
+    this.setMapWitCurrentLocation();
+  }
+
+  /** Initializes the TomTom map with current location */
+  setMapWitCurrentLocation = () => {
+    // get coordinates
+    navigator.geolocation.getCurrentPosition(async (location) => {
+      // set map with current coords
+      const script = document.createElement('script');
+      script.src = `${process.env.PUBLIC_URL}/sdk/tomtom.min.js`;
+      document.body.appendChild(script);
+      script.async = true;
+      script.onload = function () {
+        window.tomtom.L.map('map', {
+          source: 'vector',
+          key: '01ZXmKWLDr1TSBvi86xEvfIBv8DkMSX7',
+          center: [location.coords.latitude, location.coords.longitude],
+          basePath: '/sdk',
+          zoom: 16,
+        });
+      };
+    });
   }
 
 
   render() {
     return (
       <div className="map-container">
-        <div id="map"></div>
+        <div id="map" />
       </div>
-    )
-  };
+    );
+  }
 }
