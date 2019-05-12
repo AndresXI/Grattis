b0cc39e1d9e1cc6b8fbe8d245e632a0de3508777
andres alcocer <andresalcocer7@yahoo.com>
2019-05-12 15:10:17 -0400

    set up docker componse image

--- server/src/index.js
+++ server/src/index.js
@@ -6,38 +6,45 @@ import http from 'http';
 import cors from 'cors';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-import models from './models';
+import getModels from './models';
 // Merge all types from schema folder
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 // Merge all resolvers from resolver folder
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  subscriptions: {
-    onConnect: (connectionParams, webSocket, context) => {
-      console.log('connected client');
+getModels().then((models) => {
+  if (!models) {
+    console.log('could not connect to database');
+    return;
+  }
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    subscriptions: {
+      onConnect: (connectionParams, webSocket, context) => {
+        console.log('connected client');
+      },
     },
-  },
-  context: {
-    models,
-  },
-});
+    context: {
+      models,
+    },
+  });
 
 
-const app = express();
-app.use(cors('*'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-server.applyMiddleware({ app });
+  const app = express();
+  app.use(cors('*'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  server.applyMiddleware({ app });
 
-const httpServer = http.createServer(app);
-server.installSubscriptionHandlers(httpServer);
+  const httpServer = http.createServer(app);
+  server.installSubscriptionHandlers(httpServer);
 
-models.sequelize.sync({}).then(() => {
-  httpServer.listen(4001, () => {
-    console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`);
-    console.log(`🚀 Subscriptions ready at ws://localhost:4001${server.subscriptionsPath}`);
+  models.sequelize.sync({}).then(() => {
+    httpServer.listen(4001, () => {
+      console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`);
+      console.log(`🚀 Subscriptions ready at ws://localhost:4001${server.subscriptionsPath}`);
+    });
   });
 });
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,26 +1,48 @@
 import Sequelize from 'sequelize';
 
-// connect to postgres
-const sequelize = new Sequelize('grattis', 'postgres', 'barcelona10', {
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-});
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// tables
-const models = {
-  ProvidedServices: sequelize.import('./ProvidedServices'),
-  NeededServices: sequelize.import('./NeededServices'),
-  Category: sequelize.import('./Category'),
-};
 
-// Set associations
-Object.keys(models).forEach((modelName) => {
-  if ('associate' in models[modelName]) {
-    models[modelName].associate(models);
+export default async () => {
+  let maxReconnects = 20;
+  let connected = false;
+  let sequelize;
+  while (!connected && maxReconnects) {
+    // connect to postgres
+    try {
+      sequelize = new Sequelize('grattis', 'postgres', 'barcelona10', {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'postgres',
+      });
+      connected = true;
+    } catch (err) {
+      console.log('reconnecting in 5 seconds');
+      // eslit-disable-next-line no-await-in-loop
+      await sleep(5000);
+      maxReconnects -= 1;
+    }
   }
-});
 
-models.sequelize = sequelize;
-models.Sequelize = Sequelize;
+  if (!connected) {
+    return null;
+  }
 
-export default models;
+  // tables
+  const models = {
+    ProvidedServices: sequelize.import('./ProvidedServices'),
+    NeededServices: sequelize.import('./NeededServices'),
+    Category: sequelize.import('./Category'),
+  };
+
+  // Set associations
+  Object.keys(models).forEach((modelName) => {
+    if ('associate' in models[modelName]) {
+      models[modelName].associate(models);
+    }
+  });
+
+  models.sequelize = sequelize;
+  models.Sequelize = Sequelize;
+
+  return models;
+};
